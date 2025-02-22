bb544ecb838fe603b35366f8827dc9d8d5133d49
Stephen DeLorme <stephen@d.elor.me>
2025-02-22 12:01:35 -0500

    lowercase cli commands

--- littled/src/commands.rs
+++ littled/src/commands.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 use clap::Subcommand;
       
 #[derive(Debug, Clone, Deserialize, Serialize, Subcommand)]
+#[command(rename_all = "lowercase")]
 pub enum Command {
     Start { name: Option<String> },
     Stop,
