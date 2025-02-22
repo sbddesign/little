f68fc69c17eca7ffeb2dfc93a1480da98a2a9fe7
Stephen DeLorme <stephen@d.elor.me>
2025-02-22 17:44:26 -0500

    `getaddress` and `listbalances` (#6)
    * getaddress
    
    * listbalances

--- littled/src/commands.rs
+++ littled/src/commands.rs
@@ -7,11 +7,25 @@ pub enum Command {
     Start { name: Option<String> },
     Stop,
     GetInfo,
+    GetAddress,
+    ListBalances,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct GetInfoResponse {
     pub alias: String,
     pub public_key: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct GetAddressResponse {
+    pub address: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct ListBalancesResponse {
+    pub total_onchain_balance_sats: u64,
+    pub total_lightning_balance_sats: u64,
+    pub spendable_onchain_balance_sats: u64,
 }
 
